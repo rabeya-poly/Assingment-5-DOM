8d7659e9ba5c7aec7df1f0a55d4074e63678b7fd
rabeya poly <personhappy119@gmail.com>
2024-09-23 22:16:37 +0600

    done history section

--- JS/index.js
+++ JS/index.js
@@ -1,29 +1,35 @@
 let count = 0;
 
     //noakhali//
+
 document.getElementById('btn-noakhali-add-money').addEventListener('click',function(){
-    count += 1;
-    const addMoney = getInputFieldValueById('input-noakhali-add-money');
-    
-    if(addMoney>0){
-       const balance = getInputFieldTextById('noakhali-account-balance');
-       
-       const newBalance = balance + addMoney;
-       document.getElementById('noakhali-account-balance').innerText = newBalance;
-       const mainAccountBalance = getInputFieldTextById('main-account-balance');
-       const newMainAccount = mainAccountBalance - newBalance;
-       document.getElementById('main-account-balance').innerText = newMainAccount;
-         if(newMainAccount<0){
-         
-           alert('error')
-         }
+
+   const addMoney = getInputFieldValueById('input-noakhali-add-money');
+   
+   if(addMoney>0){
+      const balance = getInputFieldTextById('noakhali-account-balance');
+      
+      const newBalance = balance + addMoney;
+      document.getElementById('noakhali-account-balance').innerText = newBalance;
+      const mainAccountBalance = getInputFieldTextById('main-account-balance');
+      const newMainAccount = mainAccountBalance - newBalance;
+      if(newMainAccount<0){
+        
+        alert('error');
+        return;
+      }
+      document.getElementById('main-account-balance').innerText = newMainAccount;
        //history
-       const historyInfo = document.createElement('historyInfo');
-      historyInfo.innerText = `
-      ${addMoney}Taka is Donated for Flood-2024 at Noakhali,Bangladesh and new Balance:${newBalance}
+       const historyInfo = document.createElement('div');
+     
+       historyInfo.className = "bg-white shadow-5xl border rounded-lg w-fit p-6 m-20";
+      historyInfo.innerHTML = `
+     <p class="text-2xl w-fit "> ${addMoney}Taka is Donated for Flood-2024 at Noakhali,Bangladesh.</p>
+     <p>${new Date().toLocaleString()}</p>
       `;
-      console.log(historyInfo);
+      
       document.getElementById('history-container').appendChild(historyInfo);
+  
 }
 else{
         alert('Wrong amount.Please give right amount');
@@ -33,22 +39,29 @@ else{
 
 //feni//
 document.getElementById('btn-feni-add-money').addEventListener('click',function(){
-    const addMoney = getInputFieldValueById('input-feni-add-money');
-    
-    if(addMoney>0){
-       const balance = getInputFieldTextById('feni-account-balance');
-       
-       const newBalance = balance + addMoney;
-       document.getElementById('feni-account-balance').innerText = newBalance;
-       const mainAccountBalance = getInputFieldTextById('main-account-balance');
-       const newMainAccount = mainAccountBalance - newBalance;
-       document.getElementById('main-account-balance').innerText = newMainAccount;
+   const addMoney = getInputFieldValueById('input-feni-add-money');
+   
+   if(addMoney>0){
+      const balance = getInputFieldTextById('feni-account-balance');
+      
+      const newBalance = balance + addMoney;
+      document.getElementById('feni-account-balance').innerText = newBalance;
+      const mainAccountBalance = getInputFieldTextById('main-account-balance');
+      const newMainAccount = mainAccountBalance - newBalance;
+      if(newMainAccount<0){
+        
+        alert('error');
+        return;
+      }
+      document.getElementById('main-account-balance').innerText = newMainAccount;
        //history
-       const historyInfo = document.createElement('historyInfo');
-      historyInfo.innerText = `
-      ${addMoney}Taka is Donated for Flood Relief at Feni,Bangladesh and new Balance: ${newBalance}
+       const historyInfo = document.createElement('div');
+       historyInfo.className = "bg-white shadow-5xl border rounded-lg w-fit p-6 m-20";
+       historyInfo.innerHTML = `
+     <p class="text-2xl  w-fit"> ${addMoney}Taka is Donated for Flood Relief at Feni,Bangladesh.</p>
+     <p>${new Date().toLocaleString()}</p>
       `;
-      console.log(historyInfo);
+      
       document.getElementById('history-container').appendChild(historyInfo);
   
 }
@@ -59,7 +72,7 @@ else{
 })
 //aid//
 document.getElementById('btn-quata-add-money').addEventListener('click',function(){
-    const addMoney = getInputFieldValueById('input-quata-add-money');
+    const addMoney = getInputFieldValueById('input-quata-add-money')
     
     if(addMoney>0){
        const balance = getInputFieldTextById('quata-account-balance');
@@ -68,13 +81,20 @@ document.getElementById('btn-quata-add-money').addEventListener('click',function
        document.getElementById('quata-account-balance').innerText = newBalance;
        const mainAccountBalance = getInputFieldTextById('main-account-balance');
        const newMainAccount = mainAccountBalance - newBalance;
+       if(newMainAccount<0){
+         
+         alert('error');
+         return;
+       }
        document.getElementById('main-account-balance').innerText = newMainAccount;
        //history
-       const historyInfo = document.createElement('historyInfo');
+       const historyInfo = document.createElement('div');
+       historyInfo.className = "bg-white shadow-5xl border rounded-lg w-fit p-6 m-20";
       historyInfo.innerHTML = `
-      <p class="text-2xl">${addMoney}Taka is Donated for Aid for Injured in the Quota Movement,Bangladesh and new Balance:${newBalance}</p>
+      <p class="text-2xl  w-fit ">${addMoney}Taka is Donated for Aid for Injured in the Quota Movement,Bangladesh.</p>
+      <p>${new Date().toLocaleString()}</p>
       `;
-      console.log(historyInfo);
+      
       document.getElementById('history-container').appendChild(historyInfo);
   
 }

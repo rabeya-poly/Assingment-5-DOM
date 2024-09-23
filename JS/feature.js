3a9bb71d8dfa4768645ca2022471404bab64b719
rabeya poly <personhappy119@gmail.com>
2024-09-23 18:21:30 +0600

    added sticky top

--- JS/feature.js
+++ JS/feature.js
@@ -1,3 +1,7 @@
 document.getElementById('show-add-donation-form').addEventListener('click',function(){
   showSectionById('donation-form');
 })
+
+document.getElementById('show-history-form').addEventListener('click',function(){
+    showSectionById('history-form');
+})
--- JS/utilities.js
+++ JS/utilities.js
@@ -12,5 +12,7 @@ function getInputFieldTextById(id){
 }
 
 function showSectionById(id){
-    
+    document.getElementById('donation-form').classList.add('hidden');
+    document.getElementById('history-form').classList.add('hidden');
+    document.getElementById(id).classList.remove('hidden');
 }
